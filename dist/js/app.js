9df6dca0c4dac9f8f41bdefc763c37b9bbcd0c69
leonelnexman <reshevdev@gmail.com>
2024-08-11 11:07:01 +0300

    elkus

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,28 +1,28 @@
-new fullpage('#fullPage', {
-    autoScrolling: true,
-    navigation: true,
-    anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7'],
-    slidesNavigation: true,
-    pageFlip: true,
+// new fullpage('#fullPage', {
+//     autoScrolling: true,
+//     navigation: true,
+//     anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7'],
+//     slidesNavigation: true,
+//     pageFlip: true,
 
-    afterLoad: function(origin, destination, direction) {
-        // Запуск видео на section1
-        if (destination.anchor === 'section1') {
-            var video = document.querySelector('.main-video');
-            if (video) {
-                video.play();
-            }
-        }
+//     afterLoad: function(origin, destination, direction) {
+//         // Запуск видео на section1
+//         if (destination.anchor === 'section1') {
+//             var video = document.querySelector('.main-video');
+//             if (video) {
+//                 video.play();
+//             }
+//         }
 
-        // Добавление классов "view" элементам с классом "projects__item" на section4
-        if (destination.anchor === 'section4') {
-            var projectItems = document.querySelectorAll('.projects__item');
-            projectItems.forEach(function(item) {
-                item.classList.add('view');
-            });
-        }
-    }
-});
+//         // Добавление классов "view" элементам с классом "projects__item" на section4
+//         if (destination.anchor === 'section4') {
+//             var projectItems = document.querySelectorAll('.projects__item');
+//             projectItems.forEach(function(item) {
+//                 item.classList.add('view');
+//             });
+//         }
+//     }
+// });
 
 
 var swiper = new Swiper('.Slider', {
@@ -42,6 +42,23 @@ var swiper = new Swiper('.Slider', {
             document.querySelector('.slider-num span').textContent = currentIndex;
         }
     }
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.projects__item').forEach(item => {
+  gsap.from(item, {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    ease: 'power3.out',
+    scrollTrigger: {
+      trigger: item,
+      start: 'top 90%', // когда элемент появится на 80% высоты окна
+      toggleActions: 'play none none none', // запускает анимацию при попадании в область видимости
+    }
+  });
 });
 
 // Инициализация начального номера слайда при загрузке
