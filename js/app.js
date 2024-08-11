6105fb3c8350c97322a1a7ec521accba1c536aad
leonelnexman <reshevdev@gmail.com>
2024-08-11 11:07:39 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -84,3 +84,78 @@ var swiper = new Swiper('.custom-slider-wrapper', {
 
 // Инициализация начального номера слайда при загрузке
 document.querySelector('.slider-number span').textContent = 1;
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let mm = gsap.matchMedia();
+
+mm.add('(min-width: 769px)', () => {
+    const tl = gsap.timeline();
+
+    // Анимация прокрутки по горизонтали
+    tl.to('.service__container', {
+        x: () => -(document.querySelector('.service__container').scrollWidth - window.innerWidth)
+    });
+    
+    // Создаем ScrollTrigger для основной анимации
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: '.service__buttons',
+        start: 'top top',
+        end: '+=100%',
+        scrub: 0.5,
+        ease: 'none',
+        pin: '.service',
+        invalidateOnRefresh: true,
+    });
+
+    // Анимация для увеличения ширины элемента .line span
+    gsap.to('.line span', {
+        width: '100%', // Увеличение ширины до 100%
+        scrollTrigger: {
+            trigger: '.service__buttons',
+            start: 'top top',
+            end: '+=100%',
+            scrub: 0.5, // Связь с прокруткой
+            ease: 'none',
+            invalidateOnRefresh: true,
+        }
+    });
+});
+
+
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Удаляем класс 'active' у всех элементов с классами year1, year2, year3 и т.д.
+            document.querySelectorAll('.year1, .year2, .year3, .year4, .year5, .year6').forEach(year => {
+                year.classList.remove('active');
+            });
+
+            // Проверяем, какой элемент пересек границу, и добавляем соответствующий класс
+            if (entry.target.classList.contains('item-year1')) {
+                document.querySelector('.year1').classList.add('active');
+            } else if (entry.target.classList.contains('item-year2')) {
+                document.querySelector('.year2').classList.add('active');
+            } else if (entry.target.classList.contains('item-year3')) {
+                document.querySelector('.year3').classList.add('active');
+            } else if (entry.target.classList.contains('item-year4')) {
+                document.querySelector('.year4').classList.add('active');
+            } else if (entry.target.classList.contains('item-year5')) {
+                document.querySelector('.year5').classList.add('active');
+            } else if (entry.target.classList.contains('item-year6')) {
+                document.querySelector('.year6').classList.add('active');
+            }
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersection, {
+    threshold: 0.1
+});
+
+document.querySelectorAll('.item-year1, .item-year2, .item-year3, .item-year4, .item-year5, .item-year6')
+    .forEach(item => {
+        observer.observe(item);
+    });
